2ee5fffb3e2df810cc9f38d2a48ca31b18489af7
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-04-02 23:13:20 -0300

    Initial commit

---
+++ MeuPrimeiroProjeto/app/src/main/java/MeuPrimeiroProjeto/App.java
@@ -0,0 +1,20 @@
+package MeuPrimeiroProjeto;
+
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        int idade = leitor.nextInt();
+        
+        float cotacaoDolar = leitor.nextFloat();
+        
+        double cotacaoEuro = leitor.nextDouble();
+        
+        System.out.println("A cotacao do dolar é de ");
+        
+    }
+}
